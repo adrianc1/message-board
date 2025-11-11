ce8892d37b253a7895fb554d3434d06760bebdd4
adriansito <adriancrosby02@gmail.com>
2025-11-11 14:09:23 -0800

    CREATED get request for displaying form

--- app.js
+++ app.js
@@ -8,29 +8,15 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	const messages = [
-		{
-			text: 'Hi there!',
-			user: 'Amando',
-			added: new Date(),
-		},
-		{
-			text: 'Hello World!',
-			user: 'Charles',
-			added: new Date(),
-		},
-	];
-	res.render('index', { title: 'Mini Messageboard', messages: messages });
-});
+app.use('/', messageRouter);
 
-app.get('/new', (req, res) => {
-	res.render('form');
-});
+// app.get('/new', (req, res) => {
+// 	res.render('form');
+// });
 
-app.post('/new', (req, res) => {
-	res.status(200).render('form');
-});
+// app.post('/new', (req, res) => {
+// 	res.status(200).render('form');
+// });
 
 app.listen(PORT, () => {
 	console.log(`server running on ${PORT}`);
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,6 +1,22 @@
 const { Router } = require('express');
 const messageRouter = Router();
 
+messageRouter.get('/', (req, res) => {
+	const messages = [
+		{
+			text: 'Hi there!',
+			user: 'Amando',
+			added: new Date(),
+		},
+		{
+			text: 'Hello World!',
+			user: 'Charles',
+			added: new Date(),
+		},
+	];
+	res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
 messageRouter.get('/new', (req, res) => {
 	res.render('form');
 });
