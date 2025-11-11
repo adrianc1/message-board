ef34567e1dee80f856a9833c9d0e2c3115522aed
adriansito <adriancrosby02@gmail.com>
2025-11-11 14:33:55 -0800

    new posts are added to messages array and redirect implemented

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,19 +1,19 @@
 const { Router } = require('express');
 const messageRouter = Router();
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
 
 messageRouter.get('/', (req, res) => {
-	const messages = [
-		{
-			text: 'Hi there!',
-			user: 'Amando',
-			added: new Date(),
-		},
-		{
-			text: 'Hello World!',
-			user: 'Charles',
-			added: new Date(),
-		},
-	];
 	res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
@@ -22,7 +22,13 @@ messageRouter.get('/new', (req, res) => {
 });
 
 messageRouter.post('/new', (req, res) => {
-	res.status(200).render('form');
+	res.status(200);
+	messages.push({
+		text: req.body.message,
+		user: req.body.name,
+		added: new Date(),
+	});
+	res.redirect('/');
 });
 
 module.exports = messageRouter;
